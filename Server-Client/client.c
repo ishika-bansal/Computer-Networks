2dd3f6775a3223bf4f5fd35763c750acae3b7008
Ishika Bansal <68010668+ishika-bansal@users.noreply.github.com>
2021-04-02 18:57:15 +0530

    Add files via upload
    Network Simulation understanding the TCP protocol

---
+++ Server-Client/client.c
@@ -0,0 +1,39 @@
+#include <stdio.h> 
+#include <sys/socket.h> 
+#include <arpa/inet.h> 
+#include <unistd.h> 
+#include <string.h> 
+#define PORT 8090
+   
+int main(int argc, char const *argv[]) 
+{ 
+    int sock = 0, valread; 
+    struct sockaddr_in serv_addr; 
+    char *hello = "Hello from client"; 
+    char buffer[1024] = {0}; 
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) 
+    { 
+        printf("\n Socket creation error \n"); 
+        return -1; 
+    } 
+   
+    serv_addr.sin_family = AF_INET; 
+    //the htons() function converts values between host and network byte orders. There is a difference between big-endian and little-endian and network byte order depending on your  machine and network protocol in use.
+    serv_addr.sin_port = htons(PORT); 
+    // Convert IPv4 and IPv6 addresses from text to binary form 
+    if(inet_pton(AF_INET, "172.16.9.245", &serv_addr.sin_addr)<=0)  
+    { 
+        printf("\nInvalid address/ Address not supported \n"); 
+        return -1; 
+    } 
+    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) 
+    { 
+        printf("\nConnection Failed \n"); 
+        return -1; 
+    } 
+    send(sock , hello , strlen(hello) , 0 ); 
+    printf("Hello message sent\n"); 
+    read( sock , buffer, 1024); 
+    printf("%s\n",buffer ); 
+    return 0; 
+} 
